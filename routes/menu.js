f23610bd3bc8a06b5c25ebc91fee5e24d8d14fa6
sahandsn <sahand@greenhabitat.at>
2025-02-28 21:48:43 +0330

    fix: delete

--- routes/menu.js
+++ routes/menu.js
@@ -65,7 +65,26 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.put("/:id", validateBody(menuSchema), async (req, res) => {
+router.get("/:id", async (req, res) => {
+  try {
+    const menu = await prisma.menu.findFirst({
+      where: { userId: req.user.id, id: req.params.id },
+      include: {
+        sections: {
+          include: {
+            items: true,
+          },
+        },
+      },
+    });
+    res.json(menu);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Menu was not found" });
+  }
+});
+
+router.put("/:id", upload.any(), validateBody(menuSchema), async (req, res) => {
   try {
     const menuId = req.params.id;
     const { sections, ...menuData } = req.body;
@@ -104,7 +123,7 @@ router.put("/:id", validateBody(menuSchema), async (req, res) => {
       },
     });
 
-    res.json(updatedMenu);
+    res.json({ menu: updatedMenu, message: "Menu updated successfully!" });
   } catch (error) {
     res.status(500).json({ error: "Internal server error" });
   }
@@ -120,10 +139,29 @@ router.delete("/:id", async (req, res) => {
     });
     if (!existingMenu) return res.status(404).json({ error: "Menu not found" });
 
-    await prisma.menu.delete({ where: { id: menuId } });
-    res.status(204).end();
+    // Delete all related items and sections first
+    await prisma.$transaction([
+      prisma.item.deleteMany({
+        where: {
+          section: {
+            menuId: menuId,
+          },
+        },
+      }),
+      prisma.section.deleteMany({
+        where: {
+          menuId: menuId,
+        },
+      }),
+      prisma.menu.delete({
+        where: { id: menuId },
+      }),
+    ]);
+
+    res.status(200).json({ message: "Menu deleted successfully!" });
   } catch (error) {
-    res.status(500).json({ error: "Internal server error" });
+    console.error(error);
+    res.status(500).json({ error: "Could not delete menu" });
   }
 });
 
